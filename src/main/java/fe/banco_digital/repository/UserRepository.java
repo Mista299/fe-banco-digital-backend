4b33f1d836f71f76ff73eec57d4f434514c78706
BryanM-D <143416213+BryanM-D@users.noreply.github.com>
2026-04-02 10:35:07 -0500

    Update UserRepository.java

--- src/main/java/fe/banco_digital/repository/UserRepository.java
+++ src/main/java/fe/banco_digital/repository/UserRepository.java
@@ -3,5 +3,10 @@ package fe.banco_digital.repository;
 import fe.banco_digital.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Buscar usuario por número de identificación (opcional, útil)
+    Optional<User> findByNumeroIdentificacion(String numeroIdentificacion);
 }
