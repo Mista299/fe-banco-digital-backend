280e1b8aca2e42d8a151ba83174596b953b13abc
BryanM-D <143416213+BryanM-D@users.noreply.github.com>
2026-04-02 10:26:56 -0500

    Add files via upload

---
+++ src/main/java/fe/banco_digital/dto/BlockAccountRequest.java
@@ -0,0 +1,23 @@
+package fe.banco_digital.dto;
+
+public class BlockAccountRequest {
+
+    private Long userId;
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
