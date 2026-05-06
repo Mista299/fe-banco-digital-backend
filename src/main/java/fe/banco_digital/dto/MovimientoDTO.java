961a7d08175034d19d94c4a6ffefe9149c6c2598
Mista299 <michael.tabares@udea.edu.co>
2026-05-06 00:52:29 -0500

    test

--- src/main/java/fe/banco_digital/dto/MovimientoDTO.java
+++ src/main/java/fe/banco_digital/dto/MovimientoDTO.java
@@ -5,43 +5,39 @@ import java.time.LocalDateTime;
 
 public class MovimientoDTO {
 
+    private Long idTransaccion;
     private LocalDateTime fechaHora;
     private String concepto;
     private BigDecimal monto;
     private BigDecimal saldoResultante;
+    private String bancoDestino;
+    private String nombreReceptorExterno;
+    private String estado;
 
     public MovimientoDTO() {
     }
 
-    public LocalDateTime getFechaHora() {
-        return fechaHora;
-    }
+    public Long getIdTransaccion() { return idTransaccion; }
+    public void setIdTransaccion(Long idTransaccion) { this.idTransaccion = idTransaccion; }
 
-    public void setFechaHora(LocalDateTime fechaHora) {
-        this.fechaHora = fechaHora;
-    }
+    public LocalDateTime getFechaHora() { return fechaHora; }
+    public void setFechaHora(LocalDateTime fechaHora) { this.fechaHora = fechaHora; }
 
-    public String getConcepto() {
-        return concepto;
-    }
+    public String getConcepto() { return concepto; }
+    public void setConcepto(String concepto) { this.concepto = concepto; }
 
-    public void setConcepto(String concepto) {
-        this.concepto = concepto;
-    }
+    public BigDecimal getMonto() { return monto; }
+    public void setMonto(BigDecimal monto) { this.monto = monto; }
 
-    public BigDecimal getMonto() {
-        return monto;
-    }
+    public BigDecimal getSaldoResultante() { return saldoResultante; }
+    public void setSaldoResultante(BigDecimal saldoResultante) { this.saldoResultante = saldoResultante; }
 
-    public void setMonto(BigDecimal monto) {
-        this.monto = monto;
-    }
+    public String getBancoDestino() { return bancoDestino; }
+    public void setBancoDestino(String bancoDestino) { this.bancoDestino = bancoDestino; }
 
-    public BigDecimal getSaldoResultante() {
-        return saldoResultante;
-    }
+    public String getNombreReceptorExterno() { return nombreReceptorExterno; }
+    public void setNombreReceptorExterno(String nombreReceptorExterno) { this.nombreReceptorExterno = nombreReceptorExterno; }
 
-    public void setSaldoResultante(BigDecimal saldoResultante) {
-        this.saldoResultante = saldoResultante;
-    }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
 }
