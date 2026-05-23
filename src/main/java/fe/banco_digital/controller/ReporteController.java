31c11d9f08c8b3899998c0e9e0d40d3277ae006d
BryanM-D <143416213+BryanM-D@users.noreply.github.com>
2026-05-23 08:45:31 -0500

    Add files via upload

---
+++ src/main/java/fe/banco_digital/controller/ReporteController.java
@@ -0,0 +1,49 @@
+package fe.banco_digital.controller;
+
+import fe.banco_digital.dto.ReporteMovimientoDTO;
+import fe.banco_digital.service.ReporteService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/reportes")
+public class ReporteController {
+
+    private final ReporteService reporteService;
+
+    public ReporteController(ReporteService reporteService) {
+        this.reporteService = reporteService;
+    }
+
+    @GetMapping
+    public List<ReporteMovimientoDTO> generarReporte(
+            @RequestParam LocalDateTime inicio,
+            @RequestParam LocalDateTime fin
+    ) {
+
+        return reporteService.generarReporte(inicio, fin);
+    }
+
+    @GetMapping("/csv")
+    public ResponseEntity<byte[]> exportarCSV(
+            @RequestParam LocalDateTime inicio,
+            @RequestParam LocalDateTime fin
+    ) {
+
+        byte[] archivo =
+                reporteService.exportarCSV(inicio, fin);
+
+        return ResponseEntity.ok()
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=reporte.csv"
+                )
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(archivo);
+    }
+}
